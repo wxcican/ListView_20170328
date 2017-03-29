6a87ecf9109af47a364ba936ac736ba52bf3c7f6
wxcican <wxcican@qq.com>
2017-03-29 10:52:22 +0800

    实现ListView分页加载，实现滑动监听

--- app/src/main/java/com/fuicuiedu/xc/listview_20170328/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/listview_20170328/MainActivity.java
@@ -3,9 +3,11 @@ package com.fuicuiedu.xc.listview_20170328;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private List<String> datas;
     private ArrayAdapter<String> adapter;
     private Handler handler;
+    private int visblelastindex;//可见的最后一条数据下标
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +42,65 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);
 
-        addMoreView(listView);
+//        addMoreView(listView);
 
         listView.setAdapter(adapter);
+
+        listView.setOnScrollListener(listener);
 
 
 //        setListViewHeight(listView);
     }
+
+
+
+    //分页加载，当用户滑动到ListView底部时，自动加载数据
+//    实现：在滑动监听中，判断滑动是否静止，并且判断是否滑动到了底部（最后一条数据）
+    AbsListView.OnScrollListener listener = new AbsListView.OnScrollListener() {
+//        SCROLL_STATE_IDLE;//静止状态
+//        SCROLL_STATE_FLING;//滑翔状态（惯性滚动）
+//        SCROLL_STATE_TOUCH_SCROLL;//触摸滑动（手指摁下移动）
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+//            当滚动状态改变时触发的方法
+            //最后一条数据下标
+            int lastIndex = adapter.getCount()-1;
+//            判断滑动是否静止，并且判断是否滑动到了底部（最后一条数据）
+            if (scrollState == SCROLL_STATE_IDLE && visblelastindex == lastIndex){
+                //加载数据
+                loadMoreData();
+                adapter.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        public void onScroll(AbsListView view,
+                             int firstVisibleItem,
+                             int visibleItemCount,
+                             int totalItemCount) {
+//          正在滑动时触发的方法
+            visblelastindex = firstVisibleItem + visibleItemCount - 1;
+//      可见最后一条数据下标      可见的第一条下标        可见所有item数量
+
+
+            Log.e("===============","==============");
+            Log.e("firstVisibleItem = ","第一个可见的item: " + firstVisibleItem);//单行复制，ctrl + d
+            Log.e("visibleItemCount = ","可见的item数量:" + visibleItemCount);
+            Log.e("totalItemCount = ", "所有item数量:" + totalItemCount + "");
+            Log.e("===============","==============");
+
+
+        }
+    };
+
+
+
+
+
+
+
+
+
 
 
 
